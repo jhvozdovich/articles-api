2670012e28b9fd80ea5d88665f26cc3b7a3b0832
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-07 15:43:47 -0700

    add practice api project

---
+++ ConsoleApiCall/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApiCall
+{
+
+  public class Article
+  {
+    public string Section { get; set; }
+    public string Title { get; set; }
+    public string Abstract { get; set; }
+    public string Url { get; set; }
+    public string Byline { get; set; }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      var apiCallTask = ApiHelper.ApiCall("API KEY HERE"); //API KEY!!!!!!!!!!!!!!
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+      }
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,40 @@
+# **New York Times Articles API**
+
+#### Author: **_Jessica Hvozdovich_**
+#### June 7, 2020
+
+### Description
+
+_This application serves as an introduction to calling an API with C#/.NET Core._
+
+
+### Known Bugs
+
+No bugs have been identified at the time of this update.
+
+
+### Support and Contact Information
+
+_Have a bug or an issue with this application? [Open a new issue](https://github.com/jhvozdovich/articles-api/issues) here on GitHub._
+
+### Technologies Used
+
+* C#
+* `ASP.NET` Core
+* `ASP.NET` Core MVC
+* Entity Framework Core
+* MySQL
+* Git and GitHub
+
+### Specs
+| Spec | Input | Output |
+| :------------- | :------------- | :------------- |
+| **** | User Input:"" | Output: “” |
+No front end for specs- database practice only.
+
+
+#### License
+
+This software is licensed under the MIT license.
+
+Copyright © 2020 **_Jessica Hvozdovich_**
